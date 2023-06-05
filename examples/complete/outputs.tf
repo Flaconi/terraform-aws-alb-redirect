c8667c0f1a45b2d458a650b10e9f2eb09acb03ae
Sergey Novikov <sergey.novikov@flaconi.de>
2023-06-05 17:08:35 +0200

    Add outputs for R53 zone id

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,4 +1,9 @@
-output "redirect_this_lb_dns_name" {
-  value       = module.redirect.this_lb_dns_name
+output "redirect_dns_name" {
+  value       = module.redirect.dns_name
   description = "Application Load Balancer fqdn"
+}
+
+output "redirect_zone_id" {
+  value       = module.redirect.zone_id
+  description = "Application Load Balancer Route53 Zone ID"
 }
